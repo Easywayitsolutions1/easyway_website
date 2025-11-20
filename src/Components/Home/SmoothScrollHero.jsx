f07140dd4adda2e74f9ee1552729efcb7bbf9707
Easywayitsolutions1 <easywayitsolutions2025@gmail.com>
2025-11-20 18:00:27 +0530

    Frontend

--- src/Components/Home/SmoothScrollHero.jsx
+++ src/Components/Home/SmoothScrollHero.jsx
@@ -42,7 +42,7 @@ const Hero = () => {
 
             <ParallaxImages />
 
-            <div className="absolute bottom-0 left-0 right-0 h-96 bg-gradient-to-b from-zinc-950/0 to-zinc-950" />
+            <div className="absolute bottom-0 left-0 right-0 h-96 bg-gradient-to-b from-zinc-950/0 to-zinc-950/50" />
         </div>
     );
 };
